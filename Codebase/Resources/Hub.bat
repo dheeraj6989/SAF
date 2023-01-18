b00208cafb3a4e785f29169dad66e453df6a6c88
Baghel <dheeraj-singh.baghel@capgemini.com>
2023-01-18 12:43:20 +0530

    all doc

---
+++ Codebase/Resources/Hub.bat
@@ -0,0 +1,5 @@
+java -jar C:\\selenium-server-standalone-2.53.0.jar -role hub
+
+
+
+pause
---
+++ Codebase/Resources/node.bat
@@ -0,0 +1,4 @@
+java -jar C:\\selenium-server-standalone-2.53.0.jar -role node -Dwebdriver.chrome.driver="C:\\chromedriver.exe"  -hub http://192.168.231.158:4444/grid/register/ -port 8067
+
+
+Pause 
